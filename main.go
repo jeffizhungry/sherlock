52eae5489524cc41725d5b2ac678a46802593a54
Jeff <jeffreyh192@gmail.com>
2017-12-06 19:51:03 -0800

    Added basic proxy HTTP server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+// Proxy proxies HTTP requests
+type Proxy struct{}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	fmt.Println("Sherlock starting")
+	defer fmt.Println("Sherlock exiting...")
+	server := &Proxy{}
+	log.Fatal(http.ListenAndServe(":9999", server))
 }
