bf8b48f7a818ffeb00fa045dac0e65c8cb15864b
Jeff <jeffreyh192@gmail.com>
2018-05-19 14:16:35 -0700

    Added docs

---
+++ microapp/docs.go
@@ -0,0 +1,4 @@
+// Package microapp contains high level components of Sherlock
+// which can be ran individually or connected and ran a single
+// application connected in main.go.
+package microapp
