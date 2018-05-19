d78b9a30e9e9182fbf086200d909de386dc9078a
Jeff <jeffreyh192@gmail.com>
2018-05-19 01:20:30 -0700

    Cleaned up accumulator

--- accumulator.go
+++ accumulator.go
@@ -1,51 +1,40 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
+	"fmt"
 	"io/ioutil"
-	"net/http"
+	"net/http/httputil"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/jeffizhungry/sherlock/pkg/debug"
+	"github.com/jeffizhungry/sherlock/pkg/random"
 	"github.com/jeffizhungry/sherlock/pkg/rawhttp"
 )
 
-var WhitelistDomains = []string{}
-
 // Accumulator is responsible for taking rawhttp.Pairs and storing them
-// in the data directory, apply any filters as necessary
+// in the data directory, apply any filters as necessary. Accumulator
+// can be run concurrently as part of a worker pool.
 func Accumulator(pairs <-chan rawhttp.Pair) {
 	log := logrus.WithFields(logrus.Fields{
 		"context": "accumulator",
 	})
 	for p := range pairs {
+		r := p.Request.Method + " " + p.Request.URL.String()
+		fmt.Println("Received Raw HTTP Pair: ", r)
 
 		// Skip non json requests
 		if p.Request.Header.Get("Content-Type") != "application/json" {
+			log.WithFields(logrus.Fields{
+				"route": r,
+			}).Debug("Dropping request due to Content-Type")
 			continue
 		}
 
 		// Parse HTTP request, skip if it's not empty and invalid
 
 		// Parse HTTP response, skip if it's not empty and invalid
-
-		// Apply whitelist filter
-		if len(WhitelistDomains) > 0 {
-			host := p.Request.URL.Hostname()
-			var found bool
-			for _, d := range WhitelistDomains {
-				if d == host {
-					found = true
-					break
-				}
-			}
-			if !found {
-				continue
-			}
-		}
 
 		// Save
 		if err := savePair(p); err != nil {
@@ -61,96 +50,31 @@ func Accumulator(pairs <-chan rawhttp.Pair) {
 func savePair(p rawhttp.Pair) error {
 
 	// Make hostname directory
-	dirname := "./data/" + p.Request.URL.Hostname()
-	if err := os.MkdirAll(dirname, os.ModePerm); err != nil {
-		return err
-	}
-
-	// Save HTTP request
-	// filename := dir + "/" +  call.Timestamp.Format(time.RFC3339Nano) + "_random"
-	// if err := ioutil.WriteFile(file, data, 0664); err != nil {
-	// 	return err
-	// }
-
-	// Save HTTP response
-	// filename := dir + "/" +  call.Timestamp.Format(time.RFC3339Nano) + "_random"
-	// if err := ioutil.WriteFile(file, data, 0664); err != nil {
-	// 	return err
-	// }
-	return nil
-}
-
-func savePayload(req *http.Request, resp *http.Response) error {
-
-	// Format Data, Ignore non-JSON payloads
-	call := APIRouteCall{
-		Timestamp:  time.Now(),
-		URL:        req.URL.String(),
-		StatusCode: resp.StatusCode,
-	}
-
-	// Copy payloads
-	requestPayload, _ := ioutil.ReadAll(req.Body)
-	debug.PPrintln("request: ", string(requestPayload))
-	if err := json.Unmarshal(requestPayload, &call.RequestBody); err != nil && err != io.EOF {
-		debug.PPrintln(req)
-		// Ignore non JSON requests
-		return nil
-	}
-
-	responsePayload, _ := ioutil.ReadAll(resp.Body)
-	debug.PPrintln("reponse: ", string(responsePayload))
-	if err := json.Unmarshal(responsePayload, &call.ResponseBody); err != nil && err != io.EOF {
-		debug.PPrintln(req)
-		// Ignore non JSON responses
-		return nil
-	}
-
-	// Copy headers
-	for h := range req.Header {
-		call.RequestHeaders = append(call.RequestHeaders, h)
-	}
-	for h := range resp.Header {
-		call.ResponseHeaders = append(call.ResponseHeaders, h)
-	}
-
-	// Make Directory
-	dir := "data/" + req.URL.Hostname() + "/" + req.URL.Path
+	dir := filepath.Join(".", "data", p.Request.URL.Hostname())
 	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
 		return err
 	}
 
-	// Save
-	debug.PPrintln("saving: ", call)
-	// data, err := json.MarshalIndent(call, "", "  ")
-	// if err != nil {
-	// 	return err
-	// }
-	// if err := ioutil.WriteFile(dir + "/payload_" + call.Timestamp.Format(time.RFC3339Nano), data, 0664); err != nil {
-	// 	return err
-	// }
+	prefix := time.Now().Format(time.RFC3339Nano) + random.SafeAlphanumeric(6)
+
+	// Save HTTP request
+	reqFile := filepath.Join(dir, prefix+"_request.txt")
+	reqData, err := httputil.DumpRequest(p.Request, true)
+	if err != nil {
+		return err
+	}
+	if err = ioutil.WriteFile(reqFile, reqData, 0664); err != nil {
+		return err
+	}
+
+	// Save HTTP response
+	respFile := filepath.Join(dir, prefix+"_response.txt")
+	respData, err := httputil.DumpResponse(p.Response, true)
+	if err != nil {
+		return err
+	}
+	if err = ioutil.WriteFile(respFile, respData, 0664); err != nil {
+		return err
+	}
 	return nil
 }
-
-// func (s *Sherlock) print(req *http.Request, resp *http.Response) error {
-//
-// 	// Print Request
-// 	debug.PPrintln("request.URL:", req.URL.String())
-// 	requestBody, err := ioutil.ReadAll(req.Body)
-// 	if err != nil {
-// 		logrus.WithError(err).Error("Failed to read request body")
-// 	} else {
-// 		debug.PPrintln("request.Body:", string(requestBody))
-// 	}
-//
-// 	// Print Response
-// 	debug.PPrintln("response.Status:", resp.Status)
-// 	responseBody, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		logrus.WithError(err).Error("Failed to read resposne body")
-// 	} else {
-// 		debug.PPrintln("response.Body:", string(responseBody))
-// 	}
-// 	return nil
-// }
-//
